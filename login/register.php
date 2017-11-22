3786f79d5a95cccb47b5384470d467ac01350566
Jirawat <paoji@icloud.com>
2017-11-23 01:45:20 +0700

    Update register.php

--- login/register.php
+++ login/register.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html >
 <head>
-  <meta charset="UTF-8">
+
+  <meta http-equiv=Content-Type content="text/html; charset=utf-8">
   <title>Login Form</title>
 
 
@@ -21,6 +22,8 @@
    window.alert('รหัสประชาชนไม่ถูกต้อง');
   else header("Location: connectlogic/insertregister.php");}
   </script>
+
+
 </head>
 
 <body>
@@ -34,17 +37,26 @@
         <p>ชื่อ</p><input type="text" name="name"  required="required" />
         <p>นามสกุล</p><input type="text" name="surname"  required="required" />
         <p>วันเดือนปีเกิด</p><input type="date" name ="Date" id="myDate" value="1997-01-01">
-        <p>เลขที่บัญชีธนาคาร</p><input type="number" name="Accountnumber"  required="required" />
-        <p>ที่อยู่</p><input type="text" name="Address"  required="required" />
-        <p>อาชีพ</p><input type="text" name="Career"  required="required" />
-        <p>เบอร์โทร</p><input type="number" name="Telephone"  required="required" />
+        <p>เลขที่บัญชีธนาคาร</p><input type="number" name="Accountnumber" min="10"  required="required" />
+        <p>ธนาคาร</p>
+        <select class="w3-select w3-border" name="bank">
+        <option value="" class="label">โปรดเลือกธนาคาร</option>
+        <option value="bankOne">ธนาคารกสิกรไทย</option>
+        <option value="bankTwo">ธนาคารไทยพาณิชย์</option>
+        <option value="bankThree">ธนาคารกรุงเทพ</option>
+        <option value="bankFour">ธนาคารกรุงไทย</option>
+        <option value="bankFive">ธนาคารทหารไทย</option>
+        </select>
+        <p>ที่อยู่</p><input type="text" name="Address" min="10" required="required" />
+        <p>อาชีพ</p><input type="text" name="Career" min="5" required="required" />
+        <p>เบอร์โทร</p><input type="text" name="Telephone"  required="required" />
         <p>เงินเดือนสุดท้าย</p><input type="number" name="Salary"  required="required" />
         <p>E-mail</p><input type="text" name="Email"  required="required" />
-        <p>วันรับราชการ</p><input type="date" name="Official"  required="required" /><p>
-        <p>วันเกษียน</p><input type="date" name="Retire"  required="required" />
+        <p>วันรับราชการ</p><input type="date" name="Official"  required="required" placeholder=""/><p>
+        <p>วันเกษียน</p><input type="date" name="Retire"  required="required" placeholder="" />
         <p>จำนวนปีรับราชการ</p><input type="number" name="Bureauerucy"  required="required" />
         <button type="submit" name="submit" class="btn btn-primary btn-block btn-large">ยืนยัน</button>
-        <button type="button"   class="btn2 btn2-primary btn2-block btn2-large ">ยกเลิก</button>
+        <a><button type="button"   class="btn2 btn2-primary btn2-block btn2-large ">ยกเลิก</button></a>
 
     </form>
 </div>
